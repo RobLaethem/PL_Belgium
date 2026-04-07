ae2034c2eee2c0c35402056e7a37f8b5801054d4
RobLaethem <rob.laethem@telenet.be>
2026-04-07 12:16:41 +0200

    weightclasses

--- ipf_streamlit.py
+++ ipf_streamlit.py
@@ -6,15 +6,12 @@ st.title("Belgian powerlifting percentiles")
 df = pd.read_csv("ipf2026_belgium_raw.csv", sep=',', decimal='.', encoding='utf-8-sig')
 # convert Date to datetime
 df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
-#'Name', 'Sex', 'Event', 'Equipment', 'AgeClass', 'Division',
-#       'WeightClassKg', 'Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg',
-#       'TotalKg', 'Country', 'Date'
+
 
 #make a dropdown to select weightclass
-weight_classes = df['WeightClassKg'].unique()
+weight_classes = ["52","57","63","69","76","84","84+","59","66","74","83","93","105","120","120+"]
 selected_weight_class = st.selectbox("Select weight class", weight_classes)
 #make a dropdown to select a year
-#years = df['Date'].dt.year.unique()
 years = [2024,2025]
 selected_year = st.selectbox("Select year", years)
 #make a textbox to input a total (ensure it is a number, else ask again)
